b91acda6a01c50c74869c9533482eccb7e3ba000
Carl-Henric <carlhenric.grip@gmail.com>
2021-01-21 13:09:28 +0000

    Added coffeemaker.html and coffeemakerscript.js for brewing with coffeemaker

---
+++ assets/js/coffeemakerscript.js
@@ -0,0 +1,44 @@
+//Help from https://stackoverflow.com/questions/54637148/how-to-add-onclick-event-to-start-timer-->
+// When page is loaded Help from: https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null
+// Change cups for pour over
+window.onload=function(){
+var menu = document.getElementById("change_cups");
+menu.addEventListener("change", xcups); 
+menu.addEventListener("change", xwater);
+
+
+// Converts grams of coffe for each cup 
+    function xcups(event) {
+        let gramsofcoffee = 12;
+
+        if (menu.value == "2") {
+        return document.getElementById("coffeeweight").innerHTML = gramsofcoffee*2
+        } else if (menu.value == "3") {
+        return document.getElementById("coffeeweight").innerHTML = gramsofcoffee*3;
+        } else if (menu.value == "4") {
+        return document.getElementById("coffeeweight").innerHTML = gramsofcoffee*4;
+        } else if (menu.value == "5") {
+        return document.getElementById("coffeeweight").innerHTML = gramsofcoffee*5;
+        } else if (menu.value == "6") {
+        return document.getElementById("coffeeweight").innerHTML = gramsofcoffee*6;
+        }
+    }
+// Converts grams of water for each cup
+
+    function xwater(event) {
+        let gramsofwater = 200;
+
+        if (menu.value == "2") {
+        return document.getElementById("waterweight").innerHTML = gramsofwater*2;
+        } else if (menu.value == "3") {
+        return document.getElementById("waterweight").innerHTML = gramsofwater*3;
+        } else if (menu.value == "4") {
+        return document.getElementById("waterweight").innerHTML = gramsofwater*4;
+        } else if (menu.value == "5") {
+        return document.getElementById("waterweight").innerHTML = gramsofwater*5;
+        } else if (menu.value == "6") {
+        return document.getElementById("waterweight").innerHTML = gramsofwater*6;
+        }
+    }
+}
+    
